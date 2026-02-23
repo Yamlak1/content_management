d88da3c490aa1c8923345bcc440e01b5ebcf86e6
Yamlak Fikru <118682315+Yamlak1@users.noreply.github.com>
2026-02-23 11:12:16 +0300

    Change server port to 9836

--- src/server.js
+++ src/server.js
@@ -2,8 +2,8 @@ import { app } from "./app.js";
 import { env } from "./config/env.js";
 import { prisma } from "./config/prisma.js";
 
-const server = app.listen(env.PORT, () => {
-  console.log(`Server running on http://localhost:${env.PORT}`);
+const server = app.listen(9836, () => {
+  console.log(`Server running on http://localhost:9836`);
 });
 
 async function shutdown(signal) {
